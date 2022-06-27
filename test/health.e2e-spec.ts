600b53c8ac45e82cea4e5a6e3343341892ee2331
Dejan Radmanovic <office@decodex.net>
2022-06-27 02:09:12 +0200

    Added health e2e test

--- test/health.e2e-spec.ts
+++ test/health.e2e-spec.ts
@@ -1,21 +1,27 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
 import request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.setGlobalPrefix('api');
     await app.init();
   });
 
-  it('/api/health (GET)', () => {
-    return request(app.getHttpServer()).get('/api/health').expect(503);
+  it('/api/health (GET)', async () => {
+    return await request(app.getHttpServer()).get('/api/health').expect(503);
+  });
+
+  afterAll((done) => {
+    app.close();
+    done();
   });
 });
